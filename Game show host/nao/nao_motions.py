873cff4ff994a5c6b7bfa84badb417381f3d71d0
Mette Joëlle van Splunteren <mettejoelle@MacBook-Air-van-Mette.local>
2025-11-26 11:44:31 +0100

    motion bestand eerste versie

---
+++ Game show host/nao/nao_motions.py
@@ -0,0 +1,54 @@
+from sic_framework.core.sic_application import SICApplication
+from sic_framework.core import sic_logging
+
+# Import the device(s) we will be using
+from sic_framework.devices import Nao
+from sic_framework.devices.common_naoqi.naoqi_autonomous import NaoRestRequest
+from sic_framework.devices.common_naoqi.naoqi_leds import NaoLEDRequest
+from sic_framework.devices.nao_stub import NaoStub
+from sic_framework.devices.common_naoqi.naoqi_text_to_speech import NaoqiTextToSpeechRequest
+
+# Import message types and requests
+from sic_framework.devices.common_naoqi.naoqi_motion import (
+    NaoPostureRequest,
+    NaoqiMoveToRequest,
+    NaoqiAnimationRequest,
+)
+import time 
+
+NAO_IP = "10.0.0.137"  
+
+class NaoQuizMaster():
+
+    def __init__(self, nao_ip: str):
+        self.nao_ip = nao_ip
+        self.nao = Nao(ip=NAO_IP)
+        
+
+    def stand_up(self):
+        self.nao.motion.request(NaoPostureRequest("Stand", 0.5))
+
+    def sit_down(self):   
+        self.nao.motion.request(NaoPostureRequest("Sit", 0.5))
+    
+    def walk(self, straight, side, curve):
+        self.nao.motion.request(NaoqiMoveToRequest(x=straight, y=side, theta=curve))
+
+    def shake_head(self):
+        self.nao.tts.request(NaoqiTextToSpeechRequest("nooo you are wrong"), block=False)
+        self.nao.motion.request(NaoqiAnimationRequest("animations/Stand/Gestures/No_3"))
+	    # self.nao.tts.request(NaoqiAnimationRequest(animations/Stand/Gestures/No_3))
+
+    def run_quiz(self):
+        self.stand_up()
+        time.sleep(3)
+        self.shake_head()
+        time.sleep(5)
+        #self.sit_down()
+
+def main():
+    quiz_master = NaoQuizMaster(nao_ip=NAO_IP,)
+    quiz_master.run_quiz()
+
+if __name__ == "__main__":
+    main()

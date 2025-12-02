0eb9004ebe5105b3773af73783b15210e8388b91
Mette Joëlle van Splunteren <mettejoelle@MacBook-Air-van-Mette.local>
2025-12-02 11:18:31 +0100

    speech voice

--- Game show host/nao/nao_motions.py
+++ Game show host/nao/nao_motions.py
@@ -14,6 +14,8 @@ from sic_framework.devices.common_naoqi.naoqi_motion import (
     NaoqiMoveToRequest,
     NaoqiAnimationRequest,
 )
+from sic_framework.devices.common_naoqi.naoqi_autonomous import NaoRestRequest
+
 import time 
 
 NAO_IP = "10.0.0.137"  
@@ -30,27 +32,39 @@ class NaoQuizMaster():
 
     def sit_down(self):   
         self.nao.motion.request(NaoPostureRequest("Sit", 0.5))
-    
+
+    def rest(self):
+        self.nao.motion.request(NaoRestRequest())
+
     def walk(self, straight, side, curve):
         self.nao.motion.request(NaoqiMoveToRequest(x=straight, y=side, theta=curve))
 
     def hello_walk(self):
-        self.nao.tts.request(NaoqiTextToSpeechRequest("Hello everyone"), block=False)
-        self.nao.motion.request(NaoqiAnimationRequest("animations/Stand/Gestures/Hey_6"), block=False)
+        self.nao.tts.request(
+            NaoqiTextToSpeechRequest(
+                text="Hello everyone",
+                speed=85,              
+                pitch=1.1              
+            ),
+            block=False)
+            
+        self.nao.motion.request(NaoqiAnimationRequest("animations/Stand/Gestures/Hey_1"), block=False)
 
     def shake_head(self):
-        self.nao.tts.request(NaoqiTextToSpeechRequest("nooo you are wrong"), block=False)
+        self.nao.tts.request(
+            NaoqiTextToSpeechRequest(
+            "\\vct=110\\ \\rspd=90\\ No, you are wrong!"), block=False)        
         self.nao.motion.request(NaoqiAnimationRequest("animations/Stand/Gestures/No_3"))
 
     def run_quiz(self):
         self.stand_up()
         time.sleep(3)
         self.shake_head()
-        time.sleep(4)
-        self.hello()
+        #time.sleep(4)
+        #self.hello_walk()
 
-        time.sleep(5)
-        #self.sit_down()
+        #time.sleep(5)
+        self.rest()
 
 def main():
     quiz_master = NaoQuizMaster(nao_ip=NAO_IP,)
